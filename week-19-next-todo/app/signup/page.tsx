aa5c28f5b1c87ea65bba3fd2b3665e8c5c01b855
ashishpal07 <ashishpal7861@gmai.com>
2025-01-26 02:17:19 +0530

    Enhance signup page layout and add font variables to root layout

--- week-19-next-todo/app/signup/page.tsx
+++ week-19-next-todo/app/signup/page.tsx
@@ -17,35 +17,37 @@ function Signup() {
   }
 
   return (
-    <div className="bg-red-200 w-[30%] flex flex-col justify-center items-center p-8">
-      <div className="flex flex-col gap-y-3 w-full">
-        <input
-          className="p-2 text-black rounded-md"
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="username"
-          type="text"
-        />
-        <input
-          className="p-2 text-black rounded-md"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="user@example.com"
-          type="text"
-        />
-        <input
-          className="p-2 text-black rounded-md"
-          onChange={(e) => setpassword(e.target.value)}
-          placeholder="password"
-          type="password"
-        />
-      </div>
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col items-center p-10 bg-red-200 w-[30%] rounded-lg border border-red-200">
+        <div className="flex flex-col gap-y-5 w-full">
+          <input
+            className="p-2 text-black rounded-md"
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="username"
+            type="text"
+          />
+          <input
+            className="p-2 text-black rounded-md"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="user@example.com"
+            type="text"
+          />
+          <input
+            className="p-2 text-black rounded-md"
+            onChange={(e) => setpassword(e.target.value)}
+            placeholder="password"
+            type="password"
+          />
+        </div>
 
-      <div className="mt-5">
-        <button
-          className="border-white py-3 px-8 rounded-md bg-orange-500"
-          onClick={register}
-        >
-          signup
-        </button>
+        <div className="mt-5">
+          <button
+            className="border-white py-3 px-8 rounded-md bg-orange-500"
+            onClick={register}
+          >
+            signup
+          </button>
+        </div>
       </div>
     </div>
   );
