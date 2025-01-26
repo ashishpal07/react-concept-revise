2ef87c0c2367d1f97b6b67ce448137fe3a89c9bd
ashishpal07 <ashishpal7861@gmai.com>
2025-01-26 08:37:38 +0530

    added sigin page

--- week-19-next-todo/app/signin/page.tsx
+++ week-19-next-todo/app/signin/page.tsx
@@ -1,10 +1,41 @@
+"user client";
+
+import { useState } from "react";
 
 function Signin() {
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   return (
-    <div>
-      sign in page
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col items-center p-10 bg-red-200 w-[30%] rounded-lg border border-red-200">
+        <div className="flex flex-col gap-y-5 w-full">
+          <input
+            className="p-2 text-black rounded-md"
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="username"
+            type="text"
+          />
+          
+          <input
+            className="p-2 text-black rounded-md"
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="password"
+            type="password"
+          />
+        </div>
+
+        <div className="mt-5">
+          <button
+            className="border-white py-3 px-8 rounded-md bg-orange-500"
+          >
+            signin
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Signin;
